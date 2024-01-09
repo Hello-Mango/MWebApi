8beb739341aa4c0fa8065ad024976d4b3eaeb468
jinbo.hong <jinbo.hong@piagroup.com>
2024-01-09 14:06:15 +0800

    .....

---
+++ MWebApi/Extensions/JsonExtensions/LongToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MWebApi.Extensions.JsonExtensions
+{
+    public class LongToStringConverter : JsonConverter<long>
+    {
+        public override long Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                string stringValue = reader.GetString();
+                if (long.TryParse(stringValue, out long value))
+                {
+                    return value;
+                }
+            }
+
+            return reader.GetInt64();
+        }
+
+        public override void Write(Utf8JsonWriter writer, long value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
--- MWebApi/Program.cs
+++ MWebApi/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using MWebApi.Extensions.JsonExtensions;
 using MWebApi.Extensions.SwaggerExtensions;
 using Serilog;
 using Serilog.Events;
 using System.Text;
+using System.Text.Json;
 
 namespace MWebApi
 {
@@ -16,18 +18,21 @@ namespace MWebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            var builderC = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json");
+            //var builderC = new ConfigurationBuilder()
+            //.SetBasePath(Directory.GetCurrentDirectory())
+            //.AddJsonFile("appsettings.json");
 
-            IConfiguration configuration = builderC.Build();
-
+            //IConfiguration configuration = builderC.Build();
+            IConfiguration configuration = builder.Configuration;
             List<SwaggerGroup> swaggerGroups = new List<SwaggerGroup>()
             {
                 new SwaggerGroup("TEST1","TEST1 TITLE","TEST1 DESC"),
                 new SwaggerGroup("Hello","Hello1 TITLE","Hello1 DESC"),
             };
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new LongToStringConverter());
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwagger(swaggerGroups, true);
@@ -35,12 +40,11 @@ namespace MWebApi
             {
                 options.AddPolicy("MyPolicy", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); //MyPolicy 为自定义的策略名称，与使用时相同即可。可以同时定义多个不同策略名称的跨域策略
             });
-
+            var section = configuration.GetSection("JWTConfig");
+            string secretKey = section.GetValue<string>("SecretKey");
+            var secretByte = Encoding.UTF8.GetBytes(secretKey);
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
-                var section = configuration.GetSection("JWTConfig");
-                string secretKey = section.GetValue<string>("SecretKey");
-                var secretByte = Encoding.UTF8.GetBytes(secretKey);
                 //配置token验证
                 options.TokenValidationParameters = new TokenValidationParameters()
                 {
@@ -67,8 +71,6 @@ namespace MWebApi
             }
             app.UseCors("MyPolicy");
             app.UseAuthorization();
-
-
             app.MapControllers().RequireAuthorization();
 
             app.Run();
